a72c19dbe64725e0b115d0c65c8c6778778feb9d
Esteban Perea <mondragon1669@gmail.com>
2019-11-29 15:04:01 -0300

    Implemntação do metodo que vai imprimir o tabuleiro

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            this.qteMovimentos = 0;
+        }
+    }
+}
