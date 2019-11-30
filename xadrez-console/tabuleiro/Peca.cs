a1d0402e42399c4ec04aef1c23f179fe95d4821d
Esteban Adao Mondragon Perea <esteban.perea@webmotors.com.br>
2019-11-30 15:13:03 -0300

    Testes dos movimentos da Torre e rei

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,17 +1,17 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
-        public Tabuleiro tabuleiro { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
 
-        public Peca(Tabuleiro tabuleiro, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
             this.posicao = null;
             this.cor = cor;
-            this.tabuleiro = tabuleiro;
+            this.tab = tab;
             this.qteMovimentos = 0;
         }
 
@@ -19,5 +19,7 @@
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
